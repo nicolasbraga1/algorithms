3c694ec5894a8afa98bd48853dc1c4602622aeb6
Nícolas <nicolasbrga01@gmail.com>
2023-07-06 16:39:18 -0300

    (Feat) Req 4 - Criando função para descobrir se as palavras são anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivo = array.pop(0)
+        bigger = []
+        smaller = []
+        for word in array:
+            smaller.append(word) if word <= pivo else bigger.append(word)
+        return quicksort(smaller) + [pivo] + quicksort(bigger)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sort_first = ''.join(quicksort(list(first_string.lower())))
+    sort_second = ''.join(quicksort(list(second_string.lower())))
+    if not sort_first or not sort_second:
+        return sort_first, sort_second, False
+    else:
+        return sort_first, sort_second, sort_first == sort_second
