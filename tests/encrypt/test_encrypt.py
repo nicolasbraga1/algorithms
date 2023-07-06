bfb945e136ae05873863bec8a9ee3bd89c5c5832
Nícolas <nicolasbrga01@gmail.com>
2023-07-06 15:23:55 -0300

    (Feat) Req 2 - Implementando testes para a função de encriptar

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1989, 5)
+        encrypt_message('Nícolas', 'bla')
+    assert encrypt_message('comida', 10) == 'adimoc'
+    assert encrypt_message('alimento', 4) == 'otne_mila'
