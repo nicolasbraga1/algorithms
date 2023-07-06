4c079c1be95c3d9b1499bcaffca708357638efde
Nícolas <nicolasbrga01@gmail.com>
2023-07-06 15:42:03 -0300

    (Feat) Req 3 - Criando função recursiva para testar se a palavra é um palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if word[high_index] != word[low_index]:
+        return False
+    if high_index <= low_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
