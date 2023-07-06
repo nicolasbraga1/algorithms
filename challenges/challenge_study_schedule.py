fc4f5c8bcf9ad007f85b44126f1a4c9f50462e36
Nícolas <nicolasbrga01@gmail.com>
2023-07-06 15:10:12 -0300

    (Feat) Req 01 - Criando algoritmo de busca do horário de estudo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    student = 0
+    for login, logout in permanence_period:
+        if not isinstance(login, int) or not isinstance(logout, int):
+            return None
+        if login <= target_time <= logout:
+            student += 1
+    return student
